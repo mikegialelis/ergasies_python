b2dc1b1b0c78fdbfbe5f9395c610a8e9d5852ff1
mikegialelis <michalisgia98@hotmail.com>
2017-02-27 16:37:18 +0200

    Add files via upload

---
+++ ergasia1.py
@@ -0,0 +1,11 @@
+a=raw_input("dwse protash\n")
+a=list(a)
+for k in enumerate(a):
+    for i, j in enumerate(a):
+        try:
+            if (j  == "!") and (a[i+1]!=None) and (a[i+1]!="!") :
+                del a[i]
+        except:
+            pass
+a="".join(a)
+print a
